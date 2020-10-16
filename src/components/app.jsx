b6b06b32b4813de00c57a5c557f7da97a05de5bd
Kaushik Mahadevan <kaushik.mahadevan@gmail.com>
2020-10-16 19:32:21 -0400

    part 3 done

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,40 @@
+import { React, useState } from "react";
+
+import CreateNote from "./createNote";
 import Footer from "./footer";
 import Header from "./header";
 import Note from "./note";
-import React from "react";
 
 const App = () => {
+  const [notes, setNotes] = useState([]);
+
+  const addNote = (note) => {
+    setNotes((prevNotes) => {
+      return [...prevNotes, note];
+    });
+  };
+
+  const removeNote = (id) => {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note, index) => {
+        return index !== id;
+      });
+    });
+  };
+
   return (
     <div>
       <Header />
-      <Note title={"yuh"} content={"poopy"} />
+      <CreateNote add={addNote} />
+      {notes.map((note, index) => (
+        <Note
+          title={note.title}
+          content={note.content}
+          key={index}
+          id={index}
+          delete={removeNote}
+        />
+      ))}
       <Footer />
     </div>
   );
